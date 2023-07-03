94dad41aca62490f39acf6089438dcd44882c544
NurSultan <tnursi9898g@gmail.com>
2023-07-03 17:46:17 +0300

    completed task 4.1.3, строчка получена

---
+++ config-client/src/test/java/kz/nurs/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.nurs.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
