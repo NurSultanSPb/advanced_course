dea7f35ba83fd56273252fd2aef6c0af9bcecf67
NurSultan <tnursi9898g@gmail.com>
2023-07-14 10:53:30 +0300

    task 7.1.3 first-part done

--- client-service/src/main/java/kz/nurs/clientservice/feign/BookServiceFeign.java
+++ client-service/src/main/java/kz/nurs/clientservice/feign/BookServiceFeign.java
@@ -2,12 +2,22 @@ package kz.nurs.clientservice.feign;
 
 import kz.nurs.clientservice.model.Book;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collections;
 import java.util.List;
 
-@FeignClient(name = "book-service")
+@FeignClient(name = "book-service", fallback = BookServiceFeign.Fallback.class)
 public interface BookServiceFeign {
     @GetMapping("/api/books")
     List<Book> getAllBooks();
+
+    @Component
+    class Fallback implements BookServiceFeign {
+        @Override
+        public List<Book> getAllBooks() {
+            return Collections.emptyList();
+        }
+    }
 }
