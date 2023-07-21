e3e4f1322ba87e0b2ae9d1d25c3cdda41e26565a
NurSultan <tnursi9898g@gmail.com>
2023-07-21 14:08:57 +0300

    task 12.2.9 done с клиентом полностью

--- reactive-web-app-client/src/main/java/kz/nurs/reactivewebappclient/ReactiveWebAppClientApplication.java
+++ reactive-web-app-client/src/main/java/kz/nurs/reactivewebappclient/ReactiveWebAppClientApplication.java
@@ -1,13 +1,31 @@
 package kz.nurs.reactivewebappclient;
 
+import kz.nurs.reactivewebappclient.model.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ReactiveWebAppClientApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ReactiveWebAppClientApplication.class, args);
+        String url = "http://localhost:8080/api/books";
+        WebClient.Builder builder = WebClient.builder();
+        builder.build()
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToFlux(Book.class)
+                .subscribe(System.out::println);
+
+        builder.build()
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToFlux(Book.class)
+                .last()
+                .subscribe(s -> System.out.println("The last element is: " + s));
     }
 
 }
--- reactive-web-app-client/src/main/java/kz/nurs/reactivewebappclient/model/Book.java
+++ reactive-web-app-client/src/main/java/kz/nurs/reactivewebappclient/model/Book.java
@@ -1,6 +1,12 @@
 package kz.nurs.reactivewebappclient.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
 
     private Long id;
@@ -9,38 +15,4 @@ public class Book {
 
     private String author;
 
-    public Book() {}
-
-    public Book(String title, String author) {
-
-        this.title = title;
-        this.author = author;
-    }
-
-    public Book(Long id, String title, String author) {
-
-        this.id = id;
-        this.title = title;
-        this.author = author;
-    }
-
-    public Long getId() {
-
-        return this.id;
-    }
-
-    public void setId(Long id) {
-
-        this.id = id;
-    }
-
-    public String getTitle() {
-
-        return this.title;
-    }
-
-    public String getAuthor() {
-
-        return this.author;
-    }
 }
